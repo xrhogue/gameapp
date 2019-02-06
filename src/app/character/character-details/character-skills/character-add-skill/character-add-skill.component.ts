64c5f3ede9f076c2b45db8c8b4467a5fd70cd14a
xrhogue <xrhogue@gmail.com>
2019-02-06 07:17:37 -0700

    changed functionality to select/un select nodes based on prerequisites

--- src/app/character/character-details/character-skills/character-add-skill/character-add-skill.component.ts
+++ src/app/character/character-details/character-skills/character-add-skill/character-add-skill.component.ts
@@ -29,9 +29,9 @@ export class CharacterAddSkillComponent implements OnInit {
     this.skillService.getSkills().subscribe(skills => {
       this.skills = skills;
       this.characterSkillTreeNodes = this.buildCharacterSkillTreeNodes(null);
-      this.checkPrerequisites();
-      this.updateSelectable(null);
-      this.getCharacterTreeNodeLeaves(this.characterSkillTreeNodes).forEach(characterSkillTreeNode => this.updateSelectable(characterSkillTreeNode));
+      // this.checkPrerequisites();
+      // this.updateSelectable(null);
+      // this.getCharacterSkillTreeNodeLeaves(this.characterSkillTreeNodes).forEach(characterSkillTreeNode => this.updateSelectable(characterSkillTreeNode));
     });
   }
 
@@ -53,8 +53,6 @@ export class CharacterAddSkillComponent implements OnInit {
       characterSkillTreeNode.leaf = (characterSkillTreeNode.children == undefined || characterSkillTreeNode.children == null || characterSkillTreeNode.children.length == 0);
       characterSkillTreeNode.selectable = characterSkillTreeNode.leaf;
 
-      // TODO: set selectable state based on currently selected skills and their prerequisites.
-
       if (characterSkillTreeNode.selectable &&
         this.character.skills.find(characterSkill => characterSkill.skillId === characterSkillTreeNode.data.skillId) != null &&
         this.selectedCharacterSkillTreeNodes.find(selectedCharacterSkillTreeNode => selectedCharacterSkillTreeNode.data.skillId === characterSkillTreeNode.data.skillId) == null) {
@@ -69,10 +67,10 @@ export class CharacterAddSkillComponent implements OnInit {
     if (!characterSkillTreeNodeLeaf) {
       this.prerequisiteSkills = [];
 
-      this.skills.forEach(skill => this.updatePrerequisiteSkills(skill));
+      this.skills.forEach(skill => this.updatePrerequisiteSkills(this.getPrerequisiteSkills(), skill));
     }
     else {
-      let prerequisiteSkill: PrerequisiteSkill = this.findPrerequisiteSkill(characterSkillTreeNodeLeaf.data.skillId);
+      let prerequisiteSkill: PrerequisiteSkill = this.findPrerequisiteSkill(this.getPrerequisiteSkills(), characterSkillTreeNodeLeaf.data.skillId);
 
       if (!!prerequisiteSkill && !!prerequisiteSkill.children) {
         characterSkillTreeNodeLeaf.selectable = this.getSelectedSkillIds().filter(skillId => prerequisiteSkill.children.map(child => child.id).includes(skillId)).length == prerequisiteSkill.children.length;
@@ -85,34 +83,67 @@ export class CharacterAddSkillComponent implements OnInit {
   }
 
   characterSkillSelected(event: {node: CharacterSkillTreeNode}) {
-    this.characterSkillChanged(event);
+    this.characterSkillChanged(event, true);
   }
 
   characterSkillUnselected(event: {node: CharacterSkillTreeNode}) {
-    this.characterSkillChanged(event);
+    this.characterSkillChanged(event, false);
   }
 
-  characterSkillChanged(event: {node: CharacterSkillTreeNode}) {
-    this.checkPrerequisites();
-
-    let prerequisiteSkillIds: Array<number> = this.getSkill(event.node.data).andPrerequisiteSkillIds;
-
-    if (!!prerequisiteSkillIds) {
-      if (prerequisiteSkillIds.filter(prerequisiteSkillId => this.findPrerequisiteSkill(prerequisiteSkillId)).length < prerequisiteSkillIds.length) {
-        // TODO: remove from list... or better yet, do not allow it to be selected in the first place.
-      }
+  characterSkillChanged(event: {node: CharacterSkillTreeNode}, selected: boolean) {
+    if (selected) {
+      // TODO: sometimes prerequisite skills may be unselectable parents; in that case, we may need to select the first available descendant leaf.
+      this.updateSelectedPrerequisiteSkills(this.getSkill(event.node.data));
     }
+    else {
+      // TODO: un-select nodes if this is a prerequisite skill and there are nodes that require it.
+      this.removeSelectedDependentSkills(this.getSkill(event.node.data));
+    }
+
     this.selectedSkillNames = this.getSelectedCharacterSkills();
     this.selectedCharacterSkills = this.selectedCharacterSkillTreeNodes.map(characterSkillTreeNode => characterSkillTreeNode.data);
     this.selectedCharacterSkillsChange.emit(this.selectedCharacterSkills);
+  }
+
+  updateSelectedPrerequisiteSkills(skill: Skill) {
+    let prerequisiteSkillIds: Array<number> = skill.andPrerequisiteSkillIds;
+
+    if (!!prerequisiteSkillIds) {
+      prerequisiteSkillIds.forEach(prerequisiteSkillId => {this.updateSelectedCharacterSkillTreeNodes(prerequisiteSkillId); this.updateSelectedPrerequisiteSkills(this.getPrerequisiteSkill(prerequisiteSkillId))});
+    }
+  }
+
+  updateSelectedCharacterSkillTreeNodes(skillId: number) {
+    if (this.selectedCharacterSkillTreeNodes.find(characterSkillTreeNode => characterSkillTreeNode.data.skillId == skillId) == null) {
+      let characterSkillTreeNode: CharacterSkillTreeNode = this.getCharacterSkillTreeNode(skillId);
+
+      if (!!characterSkillTreeNode) {
+        this.selectedCharacterSkillTreeNodes.push(characterSkillTreeNode);
+      }
+    }
+  }
+
+  removeSelectedDependentSkills(skill: Skill) {
+    let prerequisiteSkill: PrerequisiteSkill = this.findPrerequisiteSkill(this.getPrerequisiteSkills(), skill.id);
+
+    if (!!prerequisiteSkill) {
+      while (!!prerequisiteSkill.parent) {
+        this.unSelectCharacterSkillTreeNode(prerequisiteSkill.parent.id);
+        prerequisiteSkill = prerequisiteSkill.parent;
+      }
+    }
+  }
+
+  unSelectCharacterSkillTreeNode(skillId: number) {
+    this.utilService.deleteObjectFromArray(this.selectedCharacterSkillTreeNodes, skillId, this.comparator);
   }
 
   checkPrerequisites() {
     this.prerequisiteSkills = [];
 
     if (!!this.selectedCharacterSkillTreeNodes) {
-      this.selectedCharacterSkillTreeNodes.forEach(selectedCharacterSkillTreeNode => this.updatePrerequisiteSkills(this.getSkill(selectedCharacterSkillTreeNode.data)));
-      this.getFlattenedPrerequisiteSkills().forEach(prerequisiteSkill => {
+      this.selectedCharacterSkillTreeNodes.forEach(selectedCharacterSkillTreeNode => this.updatePrerequisiteSkills(this.prerequisiteSkills, this.getSkill(selectedCharacterSkillTreeNode.data)));
+      this.getFlattenedPrerequisiteSkills(this.prerequisiteSkills).forEach(prerequisiteSkill => {
         if (this.selectedCharacterSkillTreeNodes.find(selectedCharacterSkillTreeNode => selectedCharacterSkillTreeNode.data.skillId == prerequisiteSkill.id) == null) {
           while (prerequisiteSkill != null) {
             this.utilService.deleteObjectFromArray(this.selectedCharacterSkillTreeNodes, prerequisiteSkill.id, this.comparator);
@@ -123,8 +154,18 @@ export class CharacterAddSkillComponent implements OnInit {
     }
   }
 
-  updatePrerequisiteSkills(skill: Skill) {
-    let prerequisiteSkill: PrerequisiteSkill = this.findPrerequisiteSkill(skill.id);
+  getPrerequisiteSkills(): Array<PrerequisiteSkill> {
+    let prerequisiteSkills: Array<PrerequisiteSkill> = [];
+
+    if (!!this.selectedCharacterSkillTreeNodes) {
+      this.selectedCharacterSkillTreeNodes.forEach(selectedCharacterSkillTreeNode => this.updatePrerequisiteSkills(prerequisiteSkills, this.getSkill(selectedCharacterSkillTreeNode.data)));
+    }
+
+    return prerequisiteSkills;
+  }
+
+  updatePrerequisiteSkills(prerequisiteSkills: Array<PrerequisiteSkill>, skill: Skill) {
+    let prerequisiteSkill: PrerequisiteSkill = this.findPrerequisiteSkill(prerequisiteSkills, skill.id);
 
     if (prerequisiteSkill != null) {
       skill.andPrerequisiteSkillIds.forEach(childPrerequisiteSkillId => prerequisiteSkill.children.push(new PrerequisiteSkill(childPrerequisiteSkillId, prerequisiteSkill, [])));
@@ -137,22 +178,25 @@ export class CharacterAddSkillComponent implements OnInit {
         prerequisiteSkill.children = skill.andPrerequisiteSkillIds.map(childPrerequisiteSkillId => new PrerequisiteSkill(childPrerequisiteSkillId, prerequisiteSkill, []));
       }
 
-      this.prerequisiteSkills.push(prerequisiteSkill);
+      prerequisiteSkills.push(prerequisiteSkill);
     }
   }
 
-  findPrerequisiteSkill(prerequisiteSkillId: number) : PrerequisiteSkill {
-    return this.flattenPrerequisiteSkills(this.prerequisiteSkills).find(prerequisiteSkill => prerequisiteSkill.id == prerequisiteSkillId);
+  findPrerequisiteSkill(prerequisiteSkills: Array<PrerequisiteSkill>, prerequisiteSkillId: number) : PrerequisiteSkill {
+    return this.getFlattenedPrerequisiteSkills(prerequisiteSkills).find(prerequisiteSkill => prerequisiteSkill.id == prerequisiteSkillId);
   }
 
-  getFlattenedPrerequisiteSkills(): Array<PrerequisiteSkill> {
-    return this.flattenPrerequisiteSkills(this.prerequisiteSkills);
-  }
-
-  flattenPrerequisiteSkills(prerequisiteSkills: Array<PrerequisiteSkill>): Array<PrerequisiteSkill> {
+  getFlattenedPrerequisiteSkills(prerequisiteSkills: Array<PrerequisiteSkill>): Array<PrerequisiteSkill> {
     let flattenedPrerequisiteSkills: Array<PrerequisiteSkill> = [];
 
-    prerequisiteSkills.map(prerequisiteSkill => {flattenedPrerequisiteSkills.push(prerequisiteSkill);flattenedPrerequisiteSkills.concat(this.flattenPrerequisiteSkills(prerequisiteSkill.children))});
+    if (!!prerequisiteSkills && prerequisiteSkills.length > 0) {
+
+      // TODO: we need to verify that the prerequisite skill is not already part of the flattened list
+      prerequisiteSkills.map(prerequisiteSkill => {
+        flattenedPrerequisiteSkills.push(prerequisiteSkill);
+        flattenedPrerequisiteSkills = flattenedPrerequisiteSkills.concat(this.getFlattenedPrerequisiteSkills(prerequisiteSkill.children));
+      });
+    }
 
     return flattenedPrerequisiteSkills;
   }
@@ -183,10 +227,14 @@ export class CharacterAddSkillComponent implements OnInit {
                .reduce((selectedSkillNames, name): string => selectedSkillNames + '\n' + name);
   }
 
-  getCharacterTreeNodeLeaves(characterSkillTreeNodes: Array<CharacterSkillTreeNode>): Array<CharacterSkillTreeNode> {
+  getCharacterSkillTreeNode(skillId: number): CharacterSkillTreeNode {
+    return this.getCharacterSkillTreeNodeLeaves(this.characterSkillTreeNodes).find(characterSkillTreeNode => characterSkillTreeNode.data.skillId == skillId);
+  }
+
+  getCharacterSkillTreeNodeLeaves(characterSkillTreeNodes: Array<CharacterSkillTreeNode>): Array<CharacterSkillTreeNode> {
     let characterSkillTreeNodeLeaves: Array<CharacterSkillTreeNode> = [];
 
-    characterSkillTreeNodes.map(characterSkillTreeNode => characterSkillTreeNode.leaf ? characterSkillTreeNodeLeaves.push(characterSkillTreeNode) : characterSkillTreeNodeLeaves.concat(this.getCharacterTreeNodeLeaves(characterSkillTreeNode.children)));
+    characterSkillTreeNodes.map(characterSkillTreeNode => characterSkillTreeNode.leaf ? characterSkillTreeNodeLeaves.push(characterSkillTreeNode) : characterSkillTreeNodeLeaves.concat(this.getCharacterSkillTreeNodeLeaves(characterSkillTreeNode.children)));
 
     return characterSkillTreeNodeLeaves;
   }
