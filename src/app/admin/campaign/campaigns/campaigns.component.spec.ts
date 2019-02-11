65cf20ad54e4ca7dfda28e413f1a48ceeb23c449
Richard Hogue <richard.hogue@incontact.com>
2019-02-11 09:03:07 -0700

    updated to Angular 7

--- src/app/admin/campaign/campaigns/campaigns.component.spec.ts
+++ src/app/admin/campaign/campaigns/campaigns.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import {} from 'jasmine';
 import { CampaignsComponent } from './campaigns.component';
 
 describe('CampaignsComponent', () => {
